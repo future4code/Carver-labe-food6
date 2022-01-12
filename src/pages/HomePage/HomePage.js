068e740f30711d456e4e4eadd5284e202287ffeb
FabioLucena <joe04_lo@hotmail.com>
2022-01-12 20:15:15 -0300

    entrega de quarta com scroll

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,123 +2,105 @@ import React, { useContext } from "react";
 import { useState } from "react";
 import RestaurantCard from "../../components/RestaurantCard/RestaurantCard";
 import GlobalContext from "../../Global/GlobalContext";
-import { ContainerPrincipal } from "./Styled";
-
+import { ContainerPrincipal} from "./Styled";
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import {NavScroll} from "./Styled";
 
 const HomePage = () => {
-    const {cont , setCont} = useState(false)
+    const { cont, setCont } = useState(false)
     const { restaurantList, setReastaurantList, restName, setRestName } = useContext(GlobalContext)
-    
+
     const newList = restaurantList
-   
-    const [search , setSearch] = useState("")
+
+    const [search, setSearch] = useState("")
     const [category, setCategory] = useState("")
 
 
     const list = restaurantList
-    .filter( restaurant =>{
-        return restaurant.name.toLowerCase().includes(search.toLowerCase())
-    })
-    .filter(restaurant=>{
-        return restaurant.category.toLowerCase().includes(category.toLowerCase())
-    })
-    .map((item)=>{
-        return(
+        .filter(restaurant => {
+            return restaurant.name.toLowerCase().includes(search.toLowerCase())
+        })
+        .filter(restaurant => {
+            return restaurant.category.toLowerCase().includes(category.toLowerCase())
+        })
+        .map((item) => {
+            return (
                 <div key={item.id}>
-                {/* <p>{item.name}</p> */}
-                <RestaurantCard 
-                rest={item}
-                />
-                </div>      
-        )
-    })
+                    {/* <p>{item.name}</p> */}
+                    <RestaurantCard
+                        rest={item}
+                    />
+                </div>
+            )
+        })
 
-    const updateSearch = (e) =>{
+    const updateSearch = (e) => {
         setSearch(e.target.value)
     }
-    
-const categoryArabe = () => {
-    setCategory("Árabe")
-}
 
-const categoryHamburguer = () => {
-    setCategory("Hamburguer")
-}
+    const categoryArabe = () => {
+        setCategory("Árabe")
+    }
 
-const categoryItaliana = () => {
-    setCategory("Italiana")
-}
+    const categoryHamburguer = () => {
+        setCategory("Hamburguer")
+    }
 
-const categorySorvetes = () => {
-    setCategory("Sorvetes")
-}
+    const categoryItaliana = () => {
+        setCategory("Italiana")
+    }
 
-const categoryCarnes = () => {
-    setCategory("Carnes")
-}
+    const categorySorvetes = () => {
+        setCategory("Sorvetes")
+    }
 
-const categoryBaiana = () => {
-    setCategory("Baiana")
-}
+    const categoryCarnes = () => {
+        setCategory("Carnes")
+    }
 
-const categoryPetiscos = () => {
-    setCategory("Petiscos")
-}
+    const categoryBaiana = () => {
+        setCategory("Baiana")
+    }
 
-const categoryAsiatica = () => {
-    setCategory("Asiática")
-}
+    const categoryPetiscos = () => {
+        setCategory("Petiscos")
+    }
 
-const categoryMexicana = () => {
-    setCategory("Mexicana")
-}
-   
-const categoryAll = () =>{
-    setCategory("")
-}
+    const categoryAsiatica = () => {
+        setCategory("Asiática")
+    }
+
+    const categoryMexicana = () => {
+        setCategory("Mexicana")
+    }
+
+    const categoryAll = () => {
+        setCategory("")
+    }
 
     return (
         <ContainerPrincipal>
             <input
-            placeholder={"Restaurante"}
-            value={search}
-            onChange={updateSearch}
+                placeholder={"Restaurante"}
+                value={search}
+                onChange={updateSearch}
             />
-            
 
-        <div class="scroll">
-            
-            <div>
-            <button onClick={categoryAll}>Todos</button>
-            </div>
-            <div>
-            <button onClick={categoryArabe}>Árabe</button>
-            </div>
-            <div>
-            <button onClick={categoryHamburguer}>Hamburguer</button>
-            </div>
-            <div>
-            <button onClick={categoryItaliana}>Italiana</button>
-            </div>
-            <div>
-            <button onClick={categorySorvetes}>Sorvetes</button>
-            </div>
-            <div>
-            <button onClick={categoryCarnes}>Carnes</button>
-            </div>
-            <div>
-            <button onClick={categoryBaiana}>Baiana</button>
-            </div>
-            <div>
-            <button onClick={categoryPetiscos}>Petiscos</button>
-            </div>
-            <div>
-            <button onClick={categoryAsiatica}>Asiática</button>
-            </div>
-            <div>
-            <button onClick={categoryMexicana}>Mexicana</button>
-            </div>
-        </div>
+
+            {/* <div class="scroll"> */}
+                <NavScroll>
+                    <button onClick={categoryAll}>Todos</button>
+                    <button onClick={categoryArabe}>Árabe</button>
+                    <button onClick={categoryHamburguer}>Hamburguer</button>
+                    <button onClick={categoryItaliana}>Italiana</button>
+                    <button onClick={categorySorvetes}>Sorvetes</button>
+                    <button onClick={categoryCarnes}>Carnes</button>
+                    <button onClick={categoryBaiana}>Baiana</button>
+                    <button onClick={categoryPetiscos}>Petiscos</button>
+                    <button onClick={categoryAsiatica}>Asiática</button>
+                    <button onClick={categoryMexicana}>Mexicana</button>
+                </NavScroll>
+            {/* </div> */}
 
             {restaurantList.length === 10 ? list : "Loading..."}
         </ContainerPrincipal>
